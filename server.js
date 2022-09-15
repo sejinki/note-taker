b317736c0962d66b0cc5f42ef327002d527c94c6
Sejin <kisejin2015@gmail.com>
2022-09-15 10:42:31 -0600

    routes

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const PORT = process.env.PORT ||  3000
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static('public'));
-
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
 app.listen(PORT,function(){
     console.log('this is running');
 })
