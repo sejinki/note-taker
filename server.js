7cea97f30cd7e3b977a91b2d133ae179eed27fe6
Sejin <kisejin2015@gmail.com>
2022-09-15 10:12:07 -0600

    apps

--- server.js
+++ server.js
@@ -1,2 +1,11 @@
 const express = require('express');
-const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT ||  3000
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT,function(){
+    console.log('this is running');
+})
