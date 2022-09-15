20b60ecf225b20b3ef369a9f1dc472dcc4939988
Sejin <kisejin2015@gmail.com>
2022-09-15 10:23:43 -0600

    routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,12 @@
+const app = require("express").Router()
+const path = require("path");
+
+app.get("/notes", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/notes.html"))
+});
+
+app.get("*", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+module.exports = app
